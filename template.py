0f0d5861580875c3f56a880e61a19de8914080e4
Abhinav_Gavireddi <anb7243@gmail.com>
2023-10-09 20:28:13 +0530

    updated modules

--- template.py
+++ template.py
@@ -2,6 +2,17 @@ import os
 from pathlib import Path
 import logging
 
+# workflow process
+# 1. update config.yaml
+# 2. update secrets.yaml
+# 3. update params.yaml
+# 4. update the entity
+# 5. update the configuration manager in src config
+# 6. update the components
+# 7. update the pipeline
+# 8. update the main.py
+# 9. update the dvc.yaml
+# 10. app.py
 
 # logging screen
 def get_logger():
